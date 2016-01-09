52761bf08a19461890acf916e47e5cdc0c8f791c
Davide Mastricci <davide.mastricci7@gmail.com>
2016-01-09 14:51:38 +0100

    create scissione

--- dipendenze/scissione.py
+++ dipendenze/scissione.py
@@ -3,7 +3,13 @@ Created on 28/dic/2015
 
 @author: pippo
 '''
-from pdfrw import PdfReader, PdfWriter 
-infile = PdfReader('bianco.pdf') 
-for i, p in enumerate(infile.pages): 
-    PdfWriter().addpage(p).write('page-%02d.pdf' % i) 
+from pdfrw import PdfReader, PdfWriter
+
+def stitching(filename):
+     if filename.endwith(".pdf"):
+        infile = PdfReader(filename)
+        for i, p in enumerate(infile.pages):
+            PdfWriter().addpage(p).write('page-%02d.pdf' % i)
+
+     else:
+        raise Exception("Errore: il file deve essere un pdf")
