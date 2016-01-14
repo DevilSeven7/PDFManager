812ef74f16fe15ba1d04f2ae7bd697b0e222e52c
DomenicoSerra <d.serra3@studenti.unisa.it>
2016-01-14 18:56:59 +0100

    uni

--- PDFManager/PDFMangerFacade.py
+++ PDFManager/PDFMangerFacade.py
@@ -4,79 +4,78 @@ import os
 
 class PDFMangerFacade:
 
-    def __init__(self):
-        None
+    def encrypt(file_name, password, filenameOut="cripted.pdf"):
 
-    def encrypt(file_name,password,a = "cripted.pdf"):
+        if(not  (isinstance(file_name,str) and isinstance(filenameOut,str)) ):
+            raise Exception('Errore: I nomi dei file devono essere stringhe')
 
-        if(a.endswith('.pdf') == False):
-            a = a+".pdf"
+        if(filenameOut.endswith('.pdf') == False):
+            filenameOut = filenameOut + ".pdf"
+
+        if(isinstance(password,str)):
+             raise Exception('Errore: La password deve essere una stringa')
 
         if(file_name.endswith(".pdf") ):
-            if(isinstance(a,str)):
-                pdf_in = open(file_name, 'rb')
-                pdf_reader = PyPDF2.PdfFileReader(pdf_in)
-                pdf_writer = PyPDF2.PdfFileWriter()
+            pdf_in = open(file_name, 'rb')
+            pdf_reader = PyPDF2.PdfFileReader(pdf_in)
+            pdf_writer = PyPDF2.PdfFileWriter()
 
-                for pagenum in range(pdf_reader.numPages):
-                    page = pdf_reader.getPage(pagenum)
-                    pdf_writer.addPage(page)
+            for pagenum in range(pdf_reader.numPages):
+                page = pdf_reader.getPage(pagenum)
+                pdf_writer.addPage(page)
 
 
-                pdf_writer.encrypt(password)
-                pdf_out = open(a, 'wb')
-                pdf_writer.write(pdf_out)
-                pdf_out.close()
-                pdf_in.close()
-            else:
-                raise Exception('Errore: il terzo parametro deve essere una stringa.')
+            pdf_writer.encrypt(password)
+            pdf_out = open(filenameOut, 'wb')
+            pdf_writer.write(pdf_out)
+            pdf_out.close()
+            pdf_in.close()
         else:
             raise Exception('Errore: Il nome del file deve terminare con .pdf')
 
-    def watermark(file_name,file_watermark,a = "newfile.pdf"):
+    def watermark(file_name, file_watermark, filenameOut="newfile.pdf"):
 
-        if(a.endswith('.pdf') == False):
-            a = a+'.pdf'
+        if(not  (isinstance(file_name,str) and isinstance(file_watermark,str) and isinstance(filenameOut,str)) ):
+            raise Exception('Errore: I nomi dei file devono essere stringhe')
+
+        if(filenameOut.endswith('.pdf') == False):
+            filenameOut = filenameOut + '.pdf'
 
         if(file_name.endswith(".pdf") and file_watermark.endswith(".pdf")):
-            if(isinstance(a,str)):
-                ipdf = PdfReader(file_name)
-                wpdf = PdfReader(file_watermark)
+            ipdf = PdfReader(file_name)
+            wpdf = PdfReader(file_watermark)
 
-                wmark = PageMerge().add(wpdf.pages[0])[0]
+            wmark = PageMerge().add(wpdf.pages[0])[0]
 
-                for page in ipdf.pages:
-                    PageMerge(page).add(wmark).render()
+            for page in ipdf.pages:
+                PageMerge(page).add(wmark).render()
 
-                PdfWriter().write(a, ipdf)
-            else:
-                raise Exception('Errore: Il terzo parametro deve essere una stringa.')
+            PdfWriter().write(filenameOut, ipdf)
         else:
             raise Exception("Errore: Il primo e il secondo parametro devono terminare con .pdf")
 
-    def merge(*varargs,a = 'merge_file'):
+    def merge(*varargs, filenameOut='merge_file'):
         if (len(varargs) <=1):
             raise Exception('Errore: utilizzare almeno due file.')
 
-        if(a.endswith('.pdf') == False):
-            a = a+".pdf"
+        if(not  (isinstance(filenameOut,str)) ):
+            raise Exception('Errore: filenameOut deve essere una stringa.')
 
-        for x in varargs:
-            if(isinstance(x,str) == False):
-                raise ValueError("Errore: Tutti i parametri devono essere stringhe.")
+        if(filenameOut.endswith('.pdf') == False):
+            filenameOut = filenameOut + ".pdf"
 
         writer = PdfWriter()
-        files = []
-        for x in varargs :
-            if x.endswith('.pdf'):
-                files.append(x)
-            else:
+
+        for fname in varargs:
+            if(isinstance(fname,str) == False):
+                raise ValueError("Errore: Tutti i parametri devono essere stringhe.")
+            if not fname.endswith('.pdf'):
                 raise Exception("Errore: tutti i parametri devono terminare con .pdf")
-        for fname in files:
+
             reader = PdfReader(fname)
             writer.addpages(reader.pages)
 
-        writer.write(a)
+        writer.write(filenameOut)
 
     def splitting(*varargs,filenameOut ="out"):
 
@@ -112,42 +111,53 @@ class PDFMangerFacade:
 
     def rotatePage(filename, filenameOut = "out", degree = 180):
 
+        if (not (isinstance(filenameOut,  str) and isinstance(filename,str))):
+            raise Exception("Errore: il nome dei file deve essere di tipo str ")
+
         if(filenameOut.endswith('.pdf') == False):
             filenameOut = filenameOut+'.pdf'
 
         if (filename.endswith(".pdf")):
-            if isinstance(filenameOut,  str):
-                if isinstance(degree, int):
-                    pdf_in = open(filename, 'rb')
-                    pdf_reader = PyPDF2.PdfFileReader(pdf_in)
-                    pdf_writer = PyPDF2.PdfFileWriter()
 
-                    for pagenum in range(pdf_reader.numPages):
-                        page = pdf_reader.getPage(pagenum)
-                        page.rotateClockwise(degree)
-                        pdf_writer.addPage(page)
+            if isinstance(degree, int):
+                pdf_in = open(filename, 'rb')
+                pdf_reader = PyPDF2.PdfFileReader(pdf_in)
+                pdf_writer = PyPDF2.PdfFileWriter()
+
+                for pagenum in range(pdf_reader.numPages):
+                    page = pdf_reader.getPage(pagenum)
+                    page.rotateClockwise(degree)
+                    pdf_writer.addPage(page)
 
 
-                    pdf_out = open(filenameOut, 'wb')
-                    pdf_writer.write(pdf_out)
-                    pdf_out.close()
-                    pdf_in.close()
-                else:
-                    raise ValueError("Errore: il grado deve essere di tipo int")
+                pdf_out = open(filenameOut, 'wb')
+                pdf_writer.write(pdf_out)
+                pdf_out.close()
+                pdf_in.close()
             else:
-                raise Exception("Errore: il nome del file di output deve essere di tipo str ")
+                raise ValueError("Errore: il grado deve essere di tipo int")
         else:
             raise Exception("Errore: il file deve essere un pdf")
 
-    def stitching(filename,fileout = 'out'):
+    def stitching(filename, filenameOut='out'):
+        if( not( isinstance(filename,str) and isinstance(filenameOut,str)) ):
+            raise Exception('Errore: I nomi dei file devono essere stringhe')
+
+        if(not filenameOut.endswith('.pdf')):
+            filenameOut = filenameOut+'.pdf'
+
         if filename.endswith(".pdf"):
             infile = PdfReader(filename)
         else:
             raise Exception("Errore: il file deve essere un pdf")
         for i, p in enumerate(infile.pages):
-            PdfWriter().addpage(p).write(fileout+'_page-%02d.pdf' % (i+1))
+            PdfWriter().addpage(p).write(filenameOut + '_page-%02d.pdf' % (i + 1))
 
     def pagescount(filename):
+
+        if( not isinstance(filename,str)):
+            raise Exception('Errore: filename deve essere una stringa')
+
         if(filename.endswith('.pdf') == False):
             raise Exception("Errore il nome del file deve terminare con .pdf")
         reader = PdfReader(filename)
